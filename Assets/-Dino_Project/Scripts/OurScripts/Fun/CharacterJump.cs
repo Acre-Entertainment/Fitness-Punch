261baee9f671a9e1b1ac73983aeef17fc386b13c
DotingU22 <88721174+DotingU22@users.noreply.github.com>
2021-09-30 13:06:55 -0300

    Mudança em alguns scripts do minigame fun

--- Assets/-Dino_Project/Scripts/OurScripts/Fun/CharacterJump.cs
+++ Assets/-Dino_Project/Scripts/OurScripts/Fun/CharacterJump.cs
@@ -4,12 +4,15 @@ using UnityEngine;
 
 public class CharacterJump : MonoBehaviour
 {
+  public int maxHealth = 3;
+  public int currentHealth;
   private Rigidbody2D rb;
   private float jumpForce = 600f;
 
   private void Start() 
   {
       rb = GetComponent<Rigidbody2D>();
+      currentHealth -= maxHealth;
   }
 
   private void Update() 
@@ -17,6 +20,14 @@ public class CharacterJump : MonoBehaviour
       if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
          {
              rb.AddForce(new Vector2(0f, jumpForce), ForceMode2D.Force);
-         }
+         } 
+  }
+
+  public void TakeDamage(int amount)
+  {
+      if(currentHealth <= 0)
+      {
+          GameControl.instance.DinoHit ();
+      }
   }
 }
