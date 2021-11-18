851b0b007ff9bffa2add812f0dd9df738e9bbc93
Fecyrineu <fcyrineu@gmail.com>
2021-11-18 18:26:35 -0300

    Grocer Minigame Results Food Sprites Implementation

--- Assets/-Dino_Project/Scripts/OurScripts/Pou/PouResultsMenu.cs
+++ Assets/-Dino_Project/Scripts/OurScripts/Pou/PouResultsMenu.cs
@@ -2,25 +2,25 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-using UnityEngine.Events;
 
 public class PouResultsMenu : MonoBehaviour
 {
     private DataHolder dh;
     private PouGameMaster pgm;
+    public Sprite[] foodSprites;
 
     [SerializeField] private GameObject iten1Button;
-    [SerializeField] private GameObject iten1Text;
-    private Text _iten1Text;
+    [SerializeField] private Image iten1Image;
+    [SerializeField] private GameObject iten1Glow;
     [SerializeField] private GameObject iten2Button;
-    [SerializeField] private GameObject iten2Text;
-    private Text _iten2Text;
+    [SerializeField] private Image iten2Image;
+    [SerializeField] private GameObject iten2Glow;
     [SerializeField] private GameObject iten3Button;
-    [SerializeField] private GameObject iten3Text;
-    private Text _iten3Text;
+    [SerializeField] private Image iten3Image;
+    [SerializeField] private GameObject iten3Glow;
     [SerializeField] private GameObject iten4Button;
-    [SerializeField] private GameObject iten4Text;
-    private Text _iten4Text;
+    [SerializeField] private Image iten4Image;
+    [SerializeField] private GameObject iten4Glow;
     [SerializeField] private GameObject inventoryFullText;
     [SerializeField] private GameObject uSuckText;
 
@@ -29,27 +29,20 @@ public class PouResultsMenu : MonoBehaviour
     private int iten3id;
     private int iten4id;
 
-    public UnityEvent OnNoInventory;
-
     void Start()
     {
         dh = GameObject.FindGameObjectWithTag("DataHolder").GetComponent<DataHolder>();
         pgm = GameObject.FindGameObjectWithTag("GameMaster").GetComponent<PouGameMaster>();
 
-        _iten1Text = iten1Text.GetComponent<Text>();
-        _iten2Text = iten2Text.GetComponent<Text>();
-        _iten3Text = iten3Text.GetComponent<Text>();
-        _iten4Text = iten4Text.GetComponent<Text>();
+        iten1id = dh.selectedGrocerFood[0] + pgm.turn1Result * 10 ;
+        iten2id = dh.selectedGrocerFood[1] + pgm.turn2Result * 10 ;
+        iten3id = dh.selectedGrocerFood[2] + pgm.turn3Result * 10 ;
+        iten4id = dh.selectedGrocerFood[3] + pgm.turn4Result * 10 ;
 
-        iten1id = dh.selectedGrocerFood[0] * 10 + pgm.turn1Result;
-        iten2id = dh.selectedGrocerFood[1] * 10 + pgm.turn2Result;
-        iten3id = dh.selectedGrocerFood[2] * 10 + pgm.turn3Result;
-        iten4id = dh.selectedGrocerFood[3] * 10 + pgm.turn4Result;
-
-        _iten1Text.text = "" + iten1id;
-        _iten2Text.text = "" + iten2id;
-        _iten3Text.text = "" + iten3id;
-        _iten4Text.text = "" + iten4id;
+        setFoodSprite(1);
+        setFoodSprite(2);
+        setFoodSprite(3);
+        setFoodSprite(4);
 
         if(pgm.turn1Result == 0){iten1Button.SetActive(false);}
         if(pgm.turn2Result == 0){iten2Button.SetActive(false);}
@@ -108,6 +101,188 @@ public class PouResultsMenu : MonoBehaviour
             iten2Button.SetActive(false);
             iten3Button.SetActive(false);
             inventoryFullText.SetActive(true);
+        }
+    }
+    private void setFoodSprite(int whichButton)
+    {
+        Image imageToChange = iten1Image;
+        GameObject glowToChange = iten1Glow;
+        int foodIDToChange = iten1id;
+        switch(whichButton)
+        {
+            case 1:
+                break;
+            case 2:
+                imageToChange = iten2Image;
+                glowToChange = iten2Glow;
+                foodIDToChange = iten2id;
+                break;
+            case 3:
+                imageToChange = iten3Image;
+                glowToChange = iten3Glow;
+                foodIDToChange = iten3id;
+                break;
+            case 4:
+                imageToChange = iten4Image;
+                glowToChange = iten4Glow;
+                foodIDToChange = iten4id;
+                break;
+        }
+        switch(foodIDToChange)
+        {
+            case 101:
+                imageToChange.sprite = foodSprites[0];
+                break;
+            case 102:
+                imageToChange.sprite = foodSprites[1];
+                break;
+            case 103:
+                imageToChange.sprite = foodSprites[2];
+                break;
+            case 104:
+                imageToChange.sprite = foodSprites[3];
+                break;
+            case 105:
+                imageToChange.sprite = foodSprites[4];
+                break;
+            case 106:
+                imageToChange.sprite = foodSprites[5];
+                break;
+            case 107:
+                imageToChange.sprite = foodSprites[6];
+                break;
+            case 108:
+                imageToChange.sprite = foodSprites[7];
+                break;
+            case 109:
+                imageToChange.sprite = foodSprites[8];
+                break;
+            case 110:
+                imageToChange.sprite = foodSprites[9];
+                break;
+            case 111:
+                imageToChange.sprite = foodSprites[10];
+                break;
+            case 112:
+                imageToChange.sprite = foodSprites[11];
+                break;
+            case 113:
+                imageToChange.sprite = foodSprites[12];
+                break;
+            case 114:
+                imageToChange.sprite = foodSprites[13];
+                break;
+            case 115:
+                imageToChange.sprite = foodSprites[14];
+                break;
+            
+            case 201:
+                imageToChange.sprite = foodSprites[15];
+                break;
+            case 202:
+                imageToChange.sprite = foodSprites[16];
+                break;
+            case 203:
+                imageToChange.sprite = foodSprites[17];
+                break;
+            case 204:
+                imageToChange.sprite = foodSprites[18];
+                break;
+            case 205:
+                imageToChange.sprite = foodSprites[19];
+                break;
+            case 206:
+                imageToChange.sprite = foodSprites[20];
+                break;
+            case 207:
+                imageToChange.sprite = foodSprites[21];
+                break;
+            case 208:
+                imageToChange.sprite = foodSprites[22];
+                break;
+            case 209:
+                imageToChange.sprite = foodSprites[23];
+                break;
+            case 210:
+                imageToChange.sprite = foodSprites[24];
+                break;
+            case 211:
+                imageToChange.sprite = foodSprites[25];
+                break;
+            case 212:
+                imageToChange.sprite = foodSprites[26];
+                break;
+            case 213:
+                imageToChange.sprite = foodSprites[27];
+                break;
+            case 214:
+                imageToChange.sprite = foodSprites[28];
+                break;
+            case 215:
+                imageToChange.sprite = foodSprites[29];
+                break;
+            
+            case 301:
+                imageToChange.sprite = foodSprites[15];
+                glowToChange.SetActive(true);
+                break;
+            case 302:
+                imageToChange.sprite = foodSprites[16];
+                glowToChange.SetActive(true);
+                break;
+            case 303:
+                imageToChange.sprite = foodSprites[17];
+                glowToChange.SetActive(true);
+                break;
+            case 304:
+                imageToChange.sprite = foodSprites[18];
+                glowToChange.SetActive(true);
+                break;
+            case 305:
+                imageToChange.sprite = foodSprites[19];
+                glowToChange.SetActive(true);
+                break;
+            case 306:
+                imageToChange.sprite = foodSprites[20];
+                glowToChange.SetActive(true);
+                break;
+            case 307:
+                imageToChange.sprite = foodSprites[21];
+                glowToChange.SetActive(true);
+                break;
+            case 308:
+                imageToChange.sprite = foodSprites[22];
+                glowToChange.SetActive(true);
+                break;
+            case 309:
+                imageToChange.sprite = foodSprites[23];
+                glowToChange.SetActive(true);
+                break;
+            case 310:
+                imageToChange.sprite = foodSprites[24];
+                glowToChange.SetActive(true);
+                break;
+            case 311:
+                imageToChange.sprite = foodSprites[25];
+                glowToChange.SetActive(true);
+                break;
+            case 312:
+                imageToChange.sprite = foodSprites[26];
+                glowToChange.SetActive(true);
+                break;
+            case 313:
+                imageToChange.sprite = foodSprites[27];
+                glowToChange.SetActive(true);
+                break;
+            case 314:
+                imageToChange.sprite = foodSprites[28];
+                glowToChange.SetActive(true);
+                break;
+            case 315:
+                imageToChange.sprite = foodSprites[29];
+                glowToChange.SetActive(true);
+                break;
+            
         }
     }
 }
