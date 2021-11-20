765a5068e86427bb1400c8c8104317f8a8b7c03d
Fecyrineu <fcyrineu@gmail.com>
2021-11-20 20:32:10 -0300

    Overworld Sleep Menu

---
+++ Assets/-Dino_Project/Scripts/OurScripts/NextDay/OverworldSleepCheck.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OverworldSleepCheck : MonoBehaviour
+{
+    private DataHolder dataHolder;
+    public GameObject sleepButton;
+    public GameObject stillActionText;
+    public GameObject theDayText;
+    public GameObject[] otherJunk;
+    void Start()
+    {
+        dataHolder = GameObject.FindGameObjectWithTag("DataHolder").GetComponent<DataHolder>();
+        if(dataHolder.dayOfTheWeek == 7)
+        {
+            foreach(GameObject go in otherJunk)
+            {
+                go.SetActive(false);
+            }
+            theDayText.SetActive(true);
+            stillActionText.SetActive(false);
+            sleepButton.SetActive(false);
+        }
+        else
+        {
+            if(dataHolder.actions == 0)
+            {
+                stillActionText.SetActive(false);
+                sleepButton.SetActive(true);
+            }
+        }
+    }
+}
