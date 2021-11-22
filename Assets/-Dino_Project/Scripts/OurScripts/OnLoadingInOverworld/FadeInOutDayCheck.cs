e0a13a00c12ad0994733a8e23c120940e600925a
Fecyrineu <fcyrineu@gmail.com>
2021-11-22 13:33:21 -0300

    Fade In/Out Adjustments

--- Assets/-Dino_Project/Scripts/OurScripts/OnLoadingInOverworld/FadeInOutDayCheck.cs
+++ Assets/-Dino_Project/Scripts/OurScripts/OnLoadingInOverworld/FadeInOutDayCheck.cs
@@ -7,11 +7,16 @@ public class FadeInOutDayCheck : MonoBehaviour
 {
     private DataHolder dataHolder;
     public Text fadeInText;
+    public GameObject fadeInObject;
     public Text fadeOutText;
     void Start()
     {
         dataHolder = GameObject.FindGameObjectWithTag("DataHolder").GetComponent<DataHolder>();
         fadeInText.text = "Dia " + dataHolder.dayOfTheWeek;
         fadeOutText.text = "Dia " + dataHolder.dayOfTheWeek + 1;
+        if(dataHolder.actions == 4)
+        {
+            fadeInObject.SetActive(true);
+        }
     }
 }
