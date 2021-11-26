672056b35e7ce11f50ef4e14867c60848d7e3ada
OppaiTaon <88720778+OppaiTaon@users.noreply.github.com>
2021-11-26 14:56:20 -0500

    MainMenu animations complete
    - tudo pegando certo
    - ainda acho bom adicionar algumas coisas, como uma animacao de menu fechando; assim como manter o titulo do game ativo durante toda a cena

--- Assets/button_Animation.cs
+++ Assets/button_Animation.cs
@@ -4,21 +4,33 @@ using UnityEngine;
 
 public class button_Animation : MonoBehaviour
 {
-    
-    void Start()
+    public GameObject enable;
+    public GameObject disable;
+
+
+    public void loadMenu()
     {
-        
+        enable.SetActive(true);
     }
 
-    
+    public void exitMenu()
+    {
+        disable.SetActive(false);
+    }
+
     public void button_touch()
     {
         GetComponent<Animation>().Play("Button_click");
     }
-    
+
     public void button_play()
     {
         GetComponent<Animation>().Play("Play_click");
     }
-   
+
+    public void exitTouch()
+    {
+        GetComponent<Animation>().Play("Exit_click");
+    }
+
 }
