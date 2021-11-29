6b4f92deace2c84022079b488f288ef53a6999f5
Fecyrineu <fcyrineu@gmail.com>
2021-11-29 18:37:20 -0300

    CoinCollecto Script Changes

--- Assets/-Dino_Project/Scripts/OurScripts/World/CoinCollector.cs
+++ Assets/-Dino_Project/Scripts/OurScripts/World/CoinCollector.cs
@@ -1,14 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CoinCollector : MonoBehaviour
 {
     private DataHolder dataHolder;
     private int coinMarker;
+    public Text[] coinText;
     void Start()
     {
         dataHolder = GameObject.FindGameObjectWithTag("DataHolder").GetComponent<DataHolder>();
+        setCoinNumberText();
     }
     void OnCollisionEnter2D(Collision2D other)
     {
@@ -18,6 +21,14 @@ public class CoinCollector : MonoBehaviour
             dataHolder.coinActivation[coinMarker] = false;
             Destroy(other.gameObject);
             dataHolder.coins++;
+            setCoinNumberText();
+        }
+    }
+    private void setCoinNumberText()
+    {
+        foreach(Text text in coinText)
+        {
+            text.text = "" + dataHolder.coins;
         }
     }
 }
