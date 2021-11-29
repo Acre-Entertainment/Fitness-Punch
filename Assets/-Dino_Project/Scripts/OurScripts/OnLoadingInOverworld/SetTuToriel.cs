59c6fabb993fac583444a4b11b326657ee15b965
Fecyrineu <fcyrineu@gmail.com>
2021-11-29 15:39:38 -0300

    TuToriel set to only open on first time

--- Assets/-Dino_Project/Scripts/OurScripts/OnLoadingInOverworld/SetTuToriel.cs
+++ Assets/-Dino_Project/Scripts/OurScripts/OnLoadingInOverworld/SetTuToriel.cs
@@ -4,15 +4,20 @@ using UnityEngine;
 
 public class SetTuToriel : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private DataHolder dataHolder;
+    public GameObject tutorial;
+    public GameObject codex;
+    public GameObject game;
     void Start()
     {
-        
+        dataHolder = GameObject.FindGameObjectWithTag("DataHolder").GetComponent<DataHolder>();
+        if(dataHolder.primeiraVezAbrindoOJogo == true)
+        {
+            tutorial.SetActive(false);
+            codex.SetActive(false);
+            game.SetActive(true);
+            dataHolder.primeiraVezAbrindoOJogo = false;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
