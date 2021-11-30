2bb89ec87a1a26be1aa40cba54a73b991bb26c1f
Fecyrineu <fcyrineu@gmail.com>
2021-11-29 21:11:28 -0300

    Overworld Lights

--- Assets/-Dino_Project/Scripts/OurScripts/OnLoadingInOverworld/CheckIfNight.cs
+++ Assets/-Dino_Project/Scripts/OurScripts/OnLoadingInOverworld/CheckIfNight.cs
@@ -6,12 +6,17 @@ public class CheckIfNight : MonoBehaviour
 {
     private DataHolder dataHolder;
     public GameObject night;
+    public GameObject[] lights;
     void Start()
     {
         dataHolder = GameObject.FindGameObjectWithTag("DataHolder").GetComponent<DataHolder>();
         if(dataHolder.actions < 3)
         {
             night.SetActive(true);
+            foreach(GameObject gameObject in lights)
+            {
+                gameObject.SetActive(true);
+            }
         }
     }
 }
